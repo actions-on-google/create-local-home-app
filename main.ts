0c2477b3eccefd7ae209383396e1f0527f7a6bf8
Johan Euphrosine <proppy@google.com>
2020-06-09 01:28:00 +0900

    create-local-home-app: update copyright notices
    - fix copyright holder
    - remove copyright notice from configs
    
    Change-Id: I2f1c0b8a12e097c4f6f18b5b10854af2d332e9da

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 /**
- * Copyright 2019, Google, Inc.
+ * Copyright 2020, Google LLC
  * Licensed under the Apache License, Version 2.0 (the 'License');
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
